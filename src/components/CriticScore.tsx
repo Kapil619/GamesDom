dc55450c765b4962d5d3649d1f5ae17a1931f9dd
kapil619 <kapilbadokar321@gmail.com>
2023-07-18 16:38:22 +0530

    Displaying score with new component

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+export const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+};
