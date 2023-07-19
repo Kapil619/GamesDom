ad4f8700c5da7ba86617af77999b66007c7deef5
kapil619 <kapilbadokar321@gmail.com>
2023-07-19 17:45:52 +0530

    Adjusted the Colors & refactored the gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,26 +12,25 @@ interface Props {
 export const GameGrid = ({ GameQuery }: Props) => {
   const { data, error, isLoading } = useGames(GameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  if (error) return <Text> {error} </Text>;
   return (
-    <>
-      {error && <Text> {error} </Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        padding={"10px"}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      padding={"10px"}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
