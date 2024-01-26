e48ec6f81fda74adf7ca3dba9aa5d06f58d690a9
kapil619 <kapilbadokar321@gmail.com>
2024-01-26 10:22:13 +0530

    New features commit (partial)

---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const GameDetailPage = () => {
+  return <div>GameDetailPage</div>;
+};
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,11 @@
+// import { Outlet } from "react-router-dom";
+// import Navbar from "../components/Navbar";
+
+// export const Layout = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//     </>
+//   );
+// };
