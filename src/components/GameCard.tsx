9a6a646ec46c40e57d90a350ec4ff4a3f168de30
Kapil619 <kapilbadokar321@gmail.com>
2024-05-23 03:13:03 +0530

    Added navigation on the whole card, Added Collapse for game description.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,19 +11,19 @@ interface Props {
 
 export const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack marginBottom={3} justifyContent={"space-between"}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize={"2xl"}>
-          <Link to={"/games/" + game.slug}>{game.name}</Link>
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={"/games/" + game.slug}>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack marginBottom={3} justifyContent={"space-between"}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,12 @@
-import { GridItem, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Button,
+  Collapse,
+  GridItem,
+  Heading,
+  SimpleGrid,
+  Spinner,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import GameAttributes from "../components/GameAttributes";
 import GameScreenshots from "../components/GameScreenshots";
@@ -8,6 +16,8 @@ import useGame from "../hooks/useGame";
 export const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
+  const [show, setShow] = useState(false);
+  const handleToggle = () => setShow(!show);
 
   if (isLoading) return <Spinner />;
 
@@ -17,7 +27,12 @@ export const GameDetailPage = () => {
     <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
       <GridItem>
         <Heading> {game.name} </Heading>
-        <Text> {game.description_raw} </Text>
+        <Collapse startingHeight={70} in={show}>
+          {game.description_raw}
+        </Collapse>
+        <Button size="sm" onClick={handleToggle} mt="1rem">
+          Show {show ? "Less" : "More"}
+        </Button>
         {/* Need to use Expandable text here */}
         <GameAttributes game={game} />
       </GridItem>
