cea9a81f69c94970fa7a298aac1cb623f3337848
Kapil619 <kapilbadokar321@gmail.com>
2024-05-22 23:54:31 +0530

    Built other game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,13 +1,17 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
     id: number;
     name: string;
     slug: string;
+    genres: Genre[];
+    publishers: Publisher[];
     description_raw: string;
     background_image: string;
-    parent_platforms: { platform: Platform; }[];
+    parent_platforms: { platform: Platform }[];
     metacritic: number;
     rating_top: number;
 }
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
+import DefinitionItem from "../components/DefinitionItem";
+import { CriticScore } from "../components/CriticScore";
+import GameAttributes from "../components/GameAttributes";
 
 export const GameDetailPage = () => {
   const { slug } = useParams();
@@ -15,6 +18,7 @@ export const GameDetailPage = () => {
     <>
       <Heading> {game.name} </Heading>
       <ExpandableText> {game.description_raw} </ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
